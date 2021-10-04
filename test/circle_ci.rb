d86489687dc235d36265102289387fcf992eba11
a6b8 <andreasbanholzer@me.com>
2021-10-04 16:20:01 +0200

    v0.01

--- test/circle_ci.rb
+++ test/circle_ci.rb
@@ -3,4 +3,10 @@ require './lib/xml_tree_viewer'
 xml = XmlTreeViewer.helper()
 XmlTreeViewer.generate( xml, :short )
 
-puts xml
+if xml.match( /Max Mustermann/ ).length != 0
+    puts "Test passed."
+    exit 0
+else
+    puts "Test not passed."
+    exit 1
+end
